53bf1fb805bf95c3896a0a46463d305880dbf292
Aram <aramsyper@gmail.com>
2018-08-08 21:17:46 +0300

    On branch master

--- Урок 1/Дополнительное задание/script.js
+++ Урок 1/Дополнительное задание/script.js
@@ -1,7 +1,5 @@
-// var num = 33721;
-// var mult = 1;
-
-// while (num>0){
-// 	mult = mult * (num%10);
-// }
-// alert(mult);
+var num = 33721;
+var mult = 3*3*7*2*1;
+var x = Math.pow(mult, 3);
+x = x+"";
+alert(x[0]+x[1]);
