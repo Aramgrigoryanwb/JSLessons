a0d45d153204dcaecc95d1daa3428df685270583
Aram <aramsyper@gmail.com>
2018-08-15 23:12:49 +0300

    correction

--- lesson5/maintust/main2/js/script.js
+++ lesson5/maintust/main2/js/script.js
@@ -1,18 +1,18 @@
 let btn = document.getElementById('open-btn');
 
-let nameValue	= document.getElementsByClassName('name-value');
-
-let budgetValue	= document.getElementsByClassName('budget-value');
+let nameValue	= document.getElementsByClassName('name-value')[0];
 	
-let goodsValue	= document.getElementsByClassName('goods-value');
-
-let itemsValue	= document.getElementsByClassName('items-value');
-
-let employersValue	= document.getElementsByClassName('employers-value');
+let budgetValue	= document.getElementsByClassName('budget-value')[0];
 	
-let discountValue	= document.getElementsByClassName('discount-value');
+let goodsValue	= document.getElementsByClassName('goods-value')[0];
 
-let isopenValue	= document.getElementsByClassName('isopen-value');
+let itemsValue	= document.getElementsByClassName('items-value')[0];
+
+let employersValue	= document.getElementsByClassName('employers-value')[0];
+	
+let discountValue	= document.getElementsByClassName('discount-value')[0];
+
+let isopenValue	= document.getElementsByClassName('isopen-value')[0];
 	
 let goodsItem = document.getElementsByClassName('goods-item');
 	
@@ -25,3 +25,9 @@ let countBudgetValue = document.querySelector('.count-budget-value');
 let timeValue = document.querySelector('.time-value');
 
 let fields = document.querySelectorAll('.hire-employers-item');
+
+// кнопки
+let openBtn = btnRight[0];	
+let confirm = btnRight[1];
+let calc = btnRight[2];
+let hires = btnRight[3];
