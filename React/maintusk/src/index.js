4785fb23f9c0f5e1ed0265d5fdf7803bbdf0bd06
Aram <aramsyper@gmail.com>
2018-09-03 23:49:37 +0300

    react

---
+++ React/maintusk/src/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import Clock from './App';
+import Button from './Button';
+import registerServiceWorker from './registerServiceWorker';
+
+
+class App extends React.Component{
+	render(){
+			return(
+				<div className="wrapper">
+					<Clock />
+					<Button />
+				</div>
+				)
+	}
+}
+
+
+ReactDOM.render(<App />, document.getElementById('root'));
+registerServiceWorker();
