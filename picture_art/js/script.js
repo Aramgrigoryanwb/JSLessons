4fa1262b7a7742eb0c34dd700305c0ad46ed4782
Aram <aramsyper@gmail.com>
2018-09-10 19:39:12 +0300

    60 sec

--- picture_art/js/script.js
+++ picture_art/js/script.js
@@ -318,3 +318,27 @@ for(let i = 0; i<accBlock.length;i++){
 		 	});
 	}
 }	
+
+//Модальное окно при посещении сайта более 60 секунд
+let startdate = new Date();
+let clockStart = startdate.getTime();
+
+function initStopwatch() {
+  let thisTime = new Date(); 
+  return (thisTime.getTime() - clockStart)/1000; 
+}
+function getSecs() {
+  var tSecs = Math.round(initStopwatch());
+  let x = setTimeout('getSecs()', 1000); 
+  console.log(x);
+  if (x>10 && x<=11){
+  	if(modalConsultation.style.display == "block" || modal.style.display == "block" || modalPopupDesign.style.display == "block"){
+  	  modalConsultation.style.display = "none";
+	}else{
+		 modalConsultation.style.display = "block";
+		document.body.style.overflow = 'hidden';
+	}
+  }
+ }
+ getSecs();
+
