1c3fad3f8d31f5e31528b3926d1f03e044284175
Aram <aramsyper@gmail.com>
2018-09-12 00:44:38 +0300

    ona est moy burger

--- picture_art/js/script.js
+++ picture_art/js/script.js
@@ -510,3 +510,24 @@ granddad.addEventListener('click',function(){
 
 
     // калькулятор
+	
+
+//Гамбургер-меню 
+ let burger = document.querySelector('.burger');
+ let burgerMenu = document.querySelector('.burger-menu');
+ let width = document.documentElement.offsetWidth;
+    
+    if(width <= 768) {
+
+     burgerMenu.style.display = 'none';
+
+       burger.addEventListener('click', function(){
+
+          if (burgerMenu.style.display == 'none') {
+                burgerMenu.style.display = 'block';
+          }else{
+                burgerMenu.style.display = 'none';
+          }
+        });
+    }
+
