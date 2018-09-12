bd24edf3b5ff40f53494d06dd0b972e766f43fe2
Aram <aramsyper@gmail.com>
2018-09-12 18:54:58 +0300

    calc final

--- picture_art/js/script.js
+++ picture_art/js/script.js
@@ -517,70 +517,65 @@ granddad.addEventListener('click',function(){
 	        coupon = document.getElementsByClassName('promocode')[0],
 	        totalValue = document.getElementsByClassName('calc-price')[0];
  		 
+            total = 0;
 			let reg = /IWANTPOPART/;
 
            
 
-           size.addEventListener('change', function(){   
-               total = (size.value * material.value * options.value);
+    size.addEventListener('change', function() {
+        total = ((size.value * material.value) + options.value);
+        if (size.value ==0 || material.value == 0) {
+            totalValue.innerHTML = '';
+        } else {
+            if (coupon.value.match(reg)) {
+                totalValue.innerHTML = total - (total*0.3);
+            } else {
+                totalValue.innerHTML = total;
+            }
+        }
+    });
 
-                if(size.value == '' ||  material.value == ''){	
-                      totalValue.innerHTML = '';
-                }else if(coupon.value.match(reg)){
-                   
-              		  totalValue.innerHTML = total - (total*0.3);
-              		}else{
+    material.addEventListener('change', function() {
+        total = ((size.value * material.value) + options.value);
+        if (size.value ==0 || material.value == 0) {
+            totalValue.innerHTML = '';
+        } else {
+            if (coupon.value.match(reg)) {
+                totalValue.innerHTML = total - (total*0.3);
+            } else {
+                totalValue.innerHTML = total;
+            }
+        }
+    });
 
-                    totalValue.innerHTML = total;
-                }
-            
-            });
-            
-           material.addEventListener('change', function(){   
-               total = (size.value * material.value * options.value);
-
-                if(size.value == '' ||  material.value == ''){  
-                      totalValue.innerHTML = '';
-                }else if(coupon.value.match(reg)){
-                   
-                    totalValue.innerHTML = total - (total*0.3);
-                  }else{
-
-                    totalValue.innerHTML = total;
-                }
-            
-            });
-
-            options.addEventListener('change', function(){   
-               total = (size.value * material.value * options.value);
-
-                if(size.value == '' ||  material.value == ''){  
-                      totalValue.innerHTML = '';
-                }else if(coupon.value.match(reg)){
-                   
-                    totalValue.innerHTML = total - (total*0.3);
-                  }else{
-
-                    totalValue.innerHTML = total;
-                }
-            
-            });
-            coupon.addEventListener('change', function(){   
-               total = (size.value * material.value * options.value);
-
-                if(size.value == '' ||  material.value == ''){  
-                      totalValue.innerHTML = '';
-                }else if(coupon.value.match(reg)){
-                   
-                    totalValue.innerHTML = total - (total*0.3);
-                  }else{
-
-                    totalValue.innerHTML = total;
-                }
-            
-            });
+    options.addEventListener('change', function() {
+        total = ((size.value * material.value) + options.value);
+        if (size.value == '' || material.value == '') {
+            totalValue.innerHTML = '';
+        } else {
+            if (coupon.value.match(reg)) {
+                totalValue.innerHTML = total - (total*0.3);
+            } else {
+                totalValue.innerHTML = total;
+            }
+        }
+    });
 
 
+    coupon.addEventListener('change', function() {
+        total = ((size.value * material.value) + options.value);
+
+        if (size.value == '' || material.value == ''){
+            totalValue.innerHTML = '';
+        } else {
+            if (coupon.value.match(reg)) {
+                totalValue.innerHTML = total - (total*0.3);
+            } else {
+                totalValue.innerHTML = total;
+            }
+        }
+
+    });
          
     
 
